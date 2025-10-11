da5e6bbd10a179a0a097d15bbb41027a1fc88729
ReiDelsAltres <velibecovv@gmail.com>
2025-10-11 22:24:04 +0400

    fix 4

--- out/404.html.js
+++ out/404.html.js
@@ -1,4 +1,5 @@
 import { Router } from "./src/foundation/worker/Router.js";
+
 Router.savePersistedRoute(window.location.pathname);
 Router.legacyRouteTo("index.html");
 //# sourceMappingURL=404.html.js.map
