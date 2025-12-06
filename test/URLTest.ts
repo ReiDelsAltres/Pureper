69009247b4c2f199924f0766834045c22f245f8e
ReiDelsAltres <velibecovv@gmail.com>
2025-12-06 16:39:47 +0400

    i

--- test/URLTest.ts
+++ test/URLTest.ts
@@ -1,11 +1,2 @@
-const str = "https://reidelsaltres.github.io/Hellper/src/components/PaperComponent.html.lt.css"
-const url = new URL("https://reidelsaltres.github.io/Hellper/chess", "https://reidelsaltres.github.io/Hellper/");
-
-console.log(url.pathname);
-
-console.log(url.origin);
-
-console.log(url.protocol);
-console.log(url.host);
-
-console.log(url.href);
+const str = "aaaa/bbbb/cccc";
+console.log(str.split('/').slice(0, 1).join('/'));
