0e70042eb0f4ba3e3c17bcebbd172b412f13cffb
ReiDelsAltres <velibecovv@gmail.com>
2025-12-08 21:32:23 +0400

    i

--- src/foundation/TripletDecorator.ts
+++ src/foundation/TripletDecorator.ts
@@ -1,21 +1,32 @@
 import { AccessType, TripletBuilder } from "./Triplet.js"
 
-export function ReComponent(html? : string, css?: string, js?: string, access?: AccessType, name?: string) {
+export function ReComponent(html? : string, css?: string, js?: string, access?: AccessType, name?: string, 
+    additionalCss?: string) {
     return (ctor: Function) => {
-        const triplet = TripletBuilder.create(html, css, js)
+        const builder = TripletBuilder.create(html, css, js)
         .withUni(ctor as any)
-        .withAccess(access ?? AccessType.BOTH)
-        .build();
+        .withAccess(access ?? AccessType.BOTH);
+
+        if (additionalCss)
+            builder.withLightDOMCss(additionalCss);
+
+        const triplet = builder.build();
 
         triplet.register("markup", name)
     }
 }
-export function RePage(html? : string, css?: string, js?: string, access?: AccessType, path?: string) {
+export function RePage(html? : string, css?: string, js?: string, access?: AccessType, path?: string,
+    additionalCss?: string
+) {
     return (ctor: Function) => {
-        const triplet = TripletBuilder.create(html, css, js)
+        const builder = TripletBuilder.create(html, css, js)
         .withUni(ctor as any)
-        .withAccess(access ?? AccessType.BOTH)
-        .build();
+        .withAccess(access ?? AccessType.BOTH);
+        
+        if (additionalCss)
+            builder.withLightDOMCss(additionalCss ?? "")
+
+        const triplet = builder.build();
 
         triplet.register("router", path)
     }
